39923b7b530550afab22e4eb873623c8a6b1369d
Anand Shukla <anands2807@gmail.com>
2021-10-29 11:52:43 +0530

    Script started

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function dragStart(){
+  console.log('dragt Start.');
+}
+
+function dragEnd(){
+  console.log('dragt End.');
+}
+
+function dragOver(){
+  console.log('dragt Over.');
+}
+
+function dragEnter(){
+  console.log('dragt Enter.');
+}
+
+function dragLeave(){
+  console.log('dragt Leave.');
+}
+
+function dragDrop(){
+  console.log('dragt Drop.');
+}
